a17c165c366a9ce1d85382528c1df1eb9c8f0d88
nickji <nickji2019@outlook.com>
2024-11-09 14:10:24 -0500

    1.0.0

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,94 @@
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-fun main() {
-    val name = "Kotlin"
-    //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-    // 查看 IntelliJ IDEA 建议如何修正。
-    println("Hello, " + name + "!")
 
-    for (i in 1 .. 5) {
-        //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-        // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-        println("i = $i")
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import org.apache.sshd.common.keyprovider.KeyPairProvider
+import org.apache.sshd.common.session.SessionContext
+import org.apache.sshd.server.SshServer
+import org.apache.sshd.server.auth.password.PasswordAuthenticator
+import java.io.FileWriter
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.security.KeyPairGenerator
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.logging.FileHandler
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
+
+val logger = Logger.getLogger("FakeSSHLogger")
+const val csvFilePath = "fakeSSH.csv"
+
+fun setupCsvFile() {
+    if (!Files.exists(Paths.get(csvFilePath))) {
+        FileWriter(csvFilePath, true).use { writer ->
+            CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("Username", "Password")).use { printer ->
+                printer.flush()
+            }
+        }
     }
+}
+
+fun logAttemptToCsv(username: String, password: String) {
+    FileWriter(csvFilePath, true).use { writer ->
+        CSVPrinter(writer, CSVFormat.DEFAULT).use { printer ->
+            printer.printRecord(username, password)
+            printer.flush()
+        }
+    }
+}
+fun setupLogger(logPath: String) {
+    val fileHandler = FileHandler(logPath, true)
+    fileHandler.formatter = SimpleFormatter()
+    logger.addHandler(fileHandler)
+}
+
+fun getCurrentTime(): String {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss-SSS")
+    return LocalDateTime.now().format(formatter)
+}
+
+fun logAttempt(address: String, username: String, password: String) {
+    logger.info("Attempt from: ${address}, Username: $username, Password: $password")
+    println("${getCurrentTime()} Attempt from: ${address}, Username: $username, Password: $password")
+    logAttemptToCsv(username, password)
+}
+val generateRandomKeyPair= {s: SessionContext -> 
+    val keyGen = KeyPairGenerator.getInstance("RSA")
+    keyGen.initialize(2048) // 设置密钥大小为2048位
+    mutableListOf(keyGen.generateKeyPair())
+}
+
+fun startSshServerWithAuth() {
+    val sshd = SshServer.setUpDefaultServer()
+    sshd.port = 2222
+    sshd.keyPairProvider = KeyPairProvider { generateRandomKeyPair(it) }
+//    sshd.serverVersion = "SSH-2.0-OpenSSH_8.0"
+
+    sshd.passwordAuthenticator = PasswordAuthenticator { username, password, session ->
+        logAttempt(session.clientAddress.toString(), username, password)
+        false
+    }
+    
+
+    try {
+        sshd.start()
+        println("Fake SSH Server started on port ${sshd.port} with a random fingerprint")
+
+        // 无限循环保持服务器运行
+        while (true) {
+            Thread.sleep(1000) // 每秒休眠一次，保持主线程不退出
+        }
+
+    } catch (e: Exception) {
+        println("Failed to start SSH server: ${e.message}")
+        e.printStackTrace()
+    } finally {
+        sshd.stop() // 在退出之前停止SSH服务器
+    }
+}
+
+fun main() {
+    setupLogger("fakeSSH.log")
+    setupCsvFile()
+    startSshServerWithAuth()
 }
